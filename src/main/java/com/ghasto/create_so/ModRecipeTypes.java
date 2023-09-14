3f308b3d80987146b288d62f936a5d0023cbe9a6
MerchantBob <122754743+MerchantBob@users.noreply.github.com>
2023-09-14 20:56:28 +0400

    Yipee! Polishing wheels work perfectly!

--- src/main/java/com/ghasto/create_so/ModRecipeTypes.java
+++ src/main/java/com/ghasto/create_so/ModRecipeTypes.java
@@ -23,17 +23,7 @@ import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraft.world.level.Level;
 
-public enum
-
-
-
-
-
-
-
-
-
-ModRecipeTypes implements IRecipeTypeInfo {
+public enum ModRecipeTypes implements IRecipeTypeInfo {
 
 	POLISHING(PolishingRecipe::new);
 
